40b287abf5540e95db805b736bba641d5c4027d5
Farahbakhsh3 <farahbakhsh3@gmail.com>
2025-10-06 14:28:02 +0330

    change version: 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="timeitPro",
-    version="0.1.1",
+    version="0.2.0",
     author="ّFarahbakhsh3",
     author_email="farahbakhsh3@gmail.com",
     description="Advanced Python function profiler with JSON logging and Flask dashboard",
